b23d375edf5ea6c365edc2e7aa51044cf2b1cf1c
Anton Arhipov <ant.arhipov@gmail.com>
2021-03-13 16:29:58 +0200

    refactoring

--- src/main/kotlin/com/example/html/Html.kt
+++ src/main/kotlin/com/example/html/Html.kt
@@ -20,34 +20,17 @@ class Html(mapBoxAccessToken: String) {
                     )
                 }
             }
-            script(ScriptType.textJavaScript) {
-                src = "https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js"
-            }
-            script(ScriptType.textJavaScript) {
-                src = "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.js"
-            }
-            script(ScriptType.textJavaScript) {
-                src = "https://api.tiles.mapbox.com/mapbox.js/plugins/turf/v2.0.0/turf.min.js"
-            }
-            script(ScriptType.textJavaScript) {
-                src = "/webjars/vue/2.1.3/vue.js"
-            }
-            script(ScriptType.textJavaScript) {
-                src = "/webjars/google-polyline/1.0.0/lib/decode.js"
-            }
-            link("/webjars/ionicons/2.0.1/css/ionicons.min.css", LinkRel.stylesheet)
-            link("https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css", LinkRel.stylesheet)
-            link(
-                "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css",
-                LinkRel.stylesheet
-            )
-            script(ScriptType.textJavaScript) {
-                src = "/assets/common.js"
-            }
-            script(ScriptType.textJavaScript) {
-                src = "/assets/driver.js"
-            }
-            link("/assets/main.css", LinkRel.stylesheet)
+            scr("https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js")
+            scr("https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.js")
+            scr("https://api.tiles.mapbox.com/mapbox.js/plugins/turf/v2.0.0/turf.min.js")
+            scr("/webjars/vue/2.1.3/vue.js")
+            scr("/webjars/google-polyline/1.0.0/lib/decode.js")
+            css("/webjars/ionicons/2.0.1/css/ionicons.min.css")
+            css("https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css")
+            css("https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css")
+            scr("/assets/common.js")
+            scr("/assets/driver.js")
+            css("/assets/main.css")
         }
         body {
             div {
@@ -64,6 +47,16 @@ class Html(mapBoxAccessToken: String) {
                     )
                 }
             }
+        }
+    }
+
+    private fun HEAD.css(source: String) {
+        link(source, LinkRel.stylesheet)
+    }
+
+    private fun HEAD.scr(source: String) {
+        script(ScriptType.textJavaScript) {
+            src = source
         }
     }
 

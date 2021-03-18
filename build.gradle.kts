0970a90d12b26cd5e0abb4eaba1e86e7a31dcb46
Viktor Gamov <viktor@confluent.io>
2021-03-18 00:40:42 -0400

    Adding docker-compose plugin to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     application
     kotlin("jvm") version "1.4.10"
     id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.avast.gradle.docker-compose") version "0.14.1"
 }
 
 group = "com.example"

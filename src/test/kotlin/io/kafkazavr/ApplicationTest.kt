2e17e763e81953790f4dfe1c63ac3ba5bd212c77
anton <ant.arhipov@gmail.com>
2023-04-10 20:36:45 +0300

    fixed imports

--- src/test/kotlin/io/kafkazavr/ApplicationTest.kt
+++ src/test/kotlin/io/kafkazavr/ApplicationTest.kt
@@ -1,18 +1,6 @@
 package io.kafkazavr
 
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.features.*
-import io.micrometer.prometheus.*
-import io.ktor.metrics.micrometer.*
-import io.ktor.http.cio.websocket.*
-import io.ktor.websocket.*
-import java.time.*
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
 import kotlin.test.*
-import io.ktor.server.testing.*
 
 class ApplicationTest {
     @Test
